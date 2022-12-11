69a9bf99fdeb04dbbe61720cfe4f5ed04d980bb0
the-future-warrior <saptakmanna17@gmail.com>
2022-12-11 11:08:18 +0530

    Create Account Page UI added...

---
+++ app/src/main/java/com/mr_17/queezy/CreateAccountActivity.kt
@@ -0,0 +1,11 @@
+package com.mr_17.queezy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
