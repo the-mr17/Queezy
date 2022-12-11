b7ae105fd7af9fde2cec21a5ac6de78c76400cf4
the-future-warrior <saptakmanna17@gmail.com>
2022-12-11 10:59:14 +0530

    Login Page UI added...

--- app/src/main/java/com/mr_17/queezy/WelcomeActivity.kt
+++ app/src/main/java/com/mr_17/queezy/WelcomeActivity.kt
@@ -1,11 +1,43 @@
 package com.mr_17.queezy
 
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private var loginButton: Button? = null
+    private var createAccountButton: Button? = null
+    private var laterButton: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        InitializeFields()
+
+        // checking clicks respective buttons and text
+        loginButton!!.setOnClickListener { SendToActivity(LoginActivity::class.java, true) }
+        createAccountButton!!.setOnClickListener { Toast.makeText(this@WelcomeActivity, "", Toast.LENGTH_LONG).show() }
+        laterButton!!.setOnClickListener { Toast.makeText(this@WelcomeActivity, getString(R.string.feature_unavailable_msg), Toast.LENGTH_LONG).show() }
+    }
+
+    private fun InitializeFields() {
+        loginButton = findViewById(R.id.login_button)
+        createAccountButton = findViewById(R.id.create_account_button)
+        laterButton = findViewById(R.id.later_button)
+    }
+
+    private fun SendToActivity(activityClass: Class<out Activity?>, backEnabled: Boolean) {
+        val intent = Intent(this, activityClass)
+        if (!backEnabled) intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+        if (!backEnabled) finish()
     }
 }
