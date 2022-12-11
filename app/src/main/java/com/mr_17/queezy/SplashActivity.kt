ce02ff1786f5e97f872ed7202a69862882bbca34
the-future-warrior <saptakmanna17@gmail.com>
2022-12-11 09:42:39 +0530

    Splash & Welcome Page UI added...

---
+++ app/src/main/java/com/mr_17/queezy/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.mr_17.queezy
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            var intent = Intent(this@SplashActivity, WelcomeActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
